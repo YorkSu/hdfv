3097d7c3f403a6ac373fe3fd7fc9529bdafa1707
YorkSu <york_su@qq.com>
2020-12-29 17:17:35 +0800

    2020-12-29 - 3

---
+++ __init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""HDFV
+======
+
+Hierarchical Data Format Visitor
+"""
+
+
+__author__ = "York Su"
+__version__ = "0.1.0"
+
+
+import os as _os
+
+ROOT_PATH = _os.path.dirname(_os.path.abspath(__file__))
+
+del _os
+
