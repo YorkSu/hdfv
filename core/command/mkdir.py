b593a749b6a1b7c5aa07d5a7a1f6f3019d99a8d1
YorkSu <york_su@qq.com>
2021-01-02 02:05:34 +0800

    Create mkdir.py

---
+++ core/command/mkdir.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Mkdir HDFV Command
+======
+
+Mkdir HDFV Command
+"""
+
+
+from hdfv.core.parse.command import Command
+from hdfv.utils.flag import flags as F
+from hdfv.utils import path
+from hdfv.utils.response import Response
+
+
+class MkdirHdfv(Command):
+    """Mkdir HDFV Command
+
+    Create a Group
+    """
+    def execute(self, *args, **kwargs) -> Response:
+        response = Response()
+
+        if not args:  # empty argument
+            response.message = "mkdir: missing operand"
+        else:
+            new_group = path.change(F.hdfv['pwd'], args[0])
+            code = path.is_group(F.hdfv['h5'], new_group)
+            if code in [200, 403]:
+                response.message = f"mkdir: cannot create directory ‘{args[0]}’: File exists"
+            elif code == 404:
+                F.hdfv['h5'].create_group(new_group)
+
+        if response.message:
+            print(response.message)
+        return response
+
